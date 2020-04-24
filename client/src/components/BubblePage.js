038d92d0793b54edbebc26584391310983b1792f
James <jlc6290@gmail.com>
2020-04-24 12:57:38 -0500

    Stretch attempted, addColor function works but duplicate entries are saved, server data appears to not update properly

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -8,8 +8,6 @@ import PrivateRoute from "./PrivateRoute";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
   useEffect(() => {
     axiosWithAuth()
       .get("http://localhost:5000/api/colors")
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
-import { useParams } from "react-router-dom";
 
 const initialColor = {
   color: "",
@@ -12,6 +11,10 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [colorToAdd, setColorToAdd] = useState({
+    color: "",
+    code: { hex: "" }
+  });
 
   const editColor = color => {
     setEditing(true);
@@ -45,6 +48,22 @@ const ColorList = ({ colors, updateColors }) => {
         console.log("delete request error: ", error);
       });
   };
+
+  const addColor = event => {
+    event.preventDefault();
+    const color = colorToAdd;
+    console.log(color);
+    axiosWithAuth()
+      .post("http://localhost:5000/api/colors", color)
+      .then(response => {
+        console.log("addColor post response: ", response);
+        // axiosWithAuth().get("http://localhost:5000/api/colors").then(response => {updateColors(response.data)})
+        updateColors(response.data)
+      })
+      .catch(error => {
+        console.log("addColor post request error: ", error);
+      })
+  }
 
   return (
     <div className="colors-wrap">
@@ -101,6 +120,28 @@ const ColorList = ({ colors, updateColors }) => {
       )}
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
+      <form onSubmit={addColor}>
+          <legend>Add a color</legend>
+          <label>color name: 
+            <input
+              type="text"
+              name="color"
+              placeholder="color name"
+              value={colorToAdd.color}
+              onChange={event => setColorToAdd({...colorToAdd, color: event.target.value})}
+            />  
+            <input
+              type="text"
+              name="hex"
+              placeholder="hex code"
+              value={colorToAdd.code.hex}
+              onChange={event => setColorToAdd({...colorToAdd, code: {hex: event.target.value}})}
+            />  
+          </label>
+          <div className="button-row">
+            <button type="submit">Add</button>
+          </div>
+      </form>
     </div>
   );
 };
